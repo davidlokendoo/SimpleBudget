c3ebd23e5f6c6691027677730172846391e7ce7c
Jose David Germosen <joseg1@MacBook-Pro.local>
2023-11-02 16:48:34 -0400

    I deleted unnecessary files and I included Budget.py

--- Budget.py
+++ Budget.py
@@ -1,22 +1,12 @@
-import math
-
-
-income = int(input("what is your monthly income?: "))
-
-expense1 = int(input("what is your monthly expense in gas?: "))
-expense2 = int(input("What is your monthly expense in groceries? "))
-expense3 = int(input("What is your monthly expense in rent? "))
-expense4 = int(input("What is your monthly expense in subscriptions? "))
-expense5 = int(input("What is your monthly expense in restaurants? "))
-
-
-outcome = income-expense1-expense2-expense3-expense4-expense5
-
-print(f"your montly expense is: ${expense1+expense2+expense3+expense4+expense5}")
-print(f"your monthly estimated remaining is: ${outcome}")
-
-
-
-
-
-
+def get_total(numbers_seperated_by_comma):
+    numbers_list = numbers_seperated_by_comma.split(",")
+    outcome = []  #new list with values as float for calculation
+    for number in numbers_list:
+        outcome.append(float(number))   #this will convert every value in the list into a float
+    calculation_sum = sum(outcome)  #this will add together every value in the list "outcome"
+    return calculation_sum
+    
+income = get_total(input("Enter your income: "))
+expense = get_total(input("Enter your expenses: "))
+remaining = income - expense
+print(f"Your total remaining is {remaining}")
